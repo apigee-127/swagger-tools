9beb99d482941782449bb76db07e8568b9eb4118
Jeremy Whitlock <jwhitlock@apache.org>
2014-07-28 11:46:15 -0600

    Use path.require(path.join(...)) for locating controllers

--- middleware/swagger-router.js
+++ middleware/swagger-router.js
@@ -34,7 +34,7 @@ var handlerCacheFromDir = function handlerCacheFromDir (dir) {
     var controller;
 
     if (file.match(jsFileRegex)) {
-      controller = require(dir + path.delimiter + controllerName);
+      controller = require(path.resolve(path.join(dir, controllerName)));
 
       if (!_.isPlainObject(controller)) {
         throw new Error('Controller module expected to export an object: ' + path.join(dir, file));
