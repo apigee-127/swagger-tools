26c9a43f1ed06e1620a05c9430e77bec94426fde
jonaslagoni <jonas-lt@live.dk>
2019-02-13 19:42:19 +0100

    Parsed response object to the security handler.

--- middleware/swagger-security.js
+++ middleware/swagger-security.js
@@ -21,41 +21,43 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-'use strict';
+"use strict";
 
-var _ = require('lodash');
-var async = require('async');
-var debug = require('debug')('swagger-tools:middleware:security');
-var helpers = require('./helpers');
+var _ = require("lodash");
+var async = require("async");
+var debug = require("debug")("swagger-tools:middleware:security");
+var helpers = require("./helpers");
 
-var getScopeOrAPIKey = function (req, secDef, secName, secReq) {
+var getScopeOrAPIKey = function(req, secDef, secName, secReq) {
   var swaggerVersion = req.swagger.swaggerVersion;
-  var apiKeyPropName = swaggerVersion === '1.2' ? secDef.keyname : secDef.name;
-  var apiKeyLocation = swaggerVersion === '1.2' ? secDef.passAs : secDef.in;
+  var apiKeyPropName = swaggerVersion === "1.2" ? secDef.keyname : secDef.name;
+  var apiKeyLocation = swaggerVersion === "1.2" ? secDef.passAs : secDef.in;
   var scopeOrKey;
 
-  if (secDef.type === 'oauth2') {
-    if (swaggerVersion === '1.2') {
-      scopeOrKey = _.map(secReq[secName], function (scope) {
+  if (secDef.type === "oauth2") {
+    if (swaggerVersion === "1.2") {
+      scopeOrKey = _.map(secReq[secName], function(scope) {
         return scope.scope;
       });
     } else {
       scopeOrKey = secReq[secName];
     }
-  } else if (secDef.type === 'apiKey') {
-    if (apiKeyLocation === 'query') {
-      scopeOrKey = (req.query ? req.query : helpers.parseQueryString(req))[apiKeyPropName];
-    } else if (apiKeyLocation === 'header') {
+  } else if (secDef.type === "apiKey") {
+    if (apiKeyLocation === "query") {
+      scopeOrKey = (req.query ? req.query : helpers.parseQueryString(req))[
+        apiKeyPropName
+      ];
+    } else if (apiKeyLocation === "header") {
       scopeOrKey = req.headers[apiKeyPropName.toLowerCase()];
     }
   }
 
   return scopeOrKey;
 };
-var sendSecurityError = function (err, res, next) {
+var sendSecurityError = function(err, res, next) {
   // Populate default values if not present
   if (!err.code) {
-    err.code = 'server_error';
+    err.code = "server_error";
   }
 
   if (!err.statusCode) {
@@ -63,7 +65,7 @@ var sendSecurityError = function (err, res, next) {
   }
 
   if (err.headers) {
-    _.each(err.headers, function (header, name) {
+    _.each(err.headers, function(header, name) {
       res.setHeader(name, header);
     });
   }
@@ -98,76 +100,107 @@ var sendSecurityError = function (err, res, next) {
  *
  * @returns the middleware function
  */
-exports = module.exports = function (options) {
+exports = module.exports = function(options) {
   var handlers = options || {};
 
-  debug('Initializing swagger-security middleware');
-  debug('  Security handlers:%s', Object.keys(handlers).length > 0 ? '' : ' ' + Object.keys(handlers).length);
+  debug("Initializing swagger-security middleware");
+  debug(
+    "  Security handlers:%s",
+    Object.keys(handlers).length > 0 ? "" : " " + Object.keys(handlers).length
+  );
 
-  _.each(options, function (func, name) {
-    debug('    %s', name);
+  _.each(options, function(func, name) {
+    debug("    %s", name);
   });
 
-  return function swaggerSecurity (req, res, next) {
+  return function swaggerSecurity(req, res, next) {
     var operation = req.swagger ? req.swagger.operation : undefined;
     var securityReqs;
 
-    debug('%s %s', req.method, req.url);
-    debug('  Will process: %s', _.isUndefined(operation) ? 'no' : 'yes');
+    debug("%s %s", req.method, req.url);
+    debug("  Will process: %s", _.isUndefined(operation) ? "no" : "yes");
 
     if (operation) {
-      securityReqs = req.swagger.swaggerVersion === '1.2' ?
-        // Global (path level), authorization support is not possible:
-        //   Not possible due to https://github.com/swagger-api/swagger-spec/issues/159
-        _.reduce(req.swagger.operation.authorizations, function (arr, authorization, name) {
-          var obj = {};
+      securityReqs =
+        req.swagger.swaggerVersion === "1.2"
+          ? // Global (path level), authorization support is not possible:
+            //   Not possible due to https://github.com/swagger-api/swagger-spec/issues/159
+            _.reduce(
+              req.swagger.operation.authorizations,
+              function(arr, authorization, name) {
+                var obj = {};
 
-          obj[name] = _.map(authorization, function (scope) {
-            return scope.scope;
-          });
+                obj[name] = _.map(authorization, function(scope) {
+                  return scope.scope;
+                });
 
-          return arr.concat(obj);
-        }, []) :
-      req.swagger.operation.security || req.swagger.swaggerObject.security;
+                return arr.concat(obj);
+              },
+              []
+            )
+          : req.swagger.operation.security ||
+            req.swagger.swaggerObject.security;
 
       if (securityReqs && securityReqs.length > 0) {
-        async.mapSeries(securityReqs, function (secReq, cb) { // logical OR - any one can allow
-          var secName;
+        async.mapSeries(
+          securityReqs,
+          function(secReq, cb) {
+            // logical OR - any one can allow
+            var secName;
 
-          async.map(Object.keys(secReq), function (name, cb) { // logical AND - all must allow
-            var secDef = req.swagger.swaggerVersion === '1.2' ?
-                  req.swagger.resourceListing.authorizations[name] :
-                  req.swagger.swaggerObject.securityDefinitions[name];
-            var handler = handlers[name];
+            async.map(
+              Object.keys(secReq),
+              function(name, cb) {
+                // logical AND - all must allow
+                var secDef =
+                  req.swagger.swaggerVersion === "1.2"
+                    ? req.swagger.resourceListing.authorizations[name]
+                    : req.swagger.swaggerObject.securityDefinitions[name];
+                var handler = handlers[name];
 
-            secName = name;
+                secName = name;
 
-            if (!handler) {
-              return cb(new Error('unknown security handler: ' + name));
+                if (!handler) {
+                  return cb(new Error("unknown security handler: " + name));
+                }
+
+                return handler(
+                  req,
+                  secDef,
+                  getScopeOrAPIKey(req, secDef, name, secReq),
+                  cb,
+                  res
+                );
+              },
+              function(err) {
+                debug(
+                  "    Security check (%s): %s",
+                  secName,
+                  _.isNull(err) ? "allowed" : "denied"
+                );
+
+                // swap normal err and result to short-circuit the logical OR
+                if (err) {
+                  return cb(undefined, err);
+                }
+
+                return cb(new Error("OK"));
+              }
+            );
+          },
+          function(ok, errors) {
+            // note swapped results
+            var allowed = !_.isNull(ok) && ok.message === "OK";
+
+            debug("    Request allowed: %s", allowed);
+
+            if (allowed) {
+              return next();
             }
 
-            return handler(req, secDef, getScopeOrAPIKey(req, secDef, name, secReq), cb);
-          }, function (err) {
-            debug('    Security check (%s): %s', secName, _.isNull(err) ? 'allowed' : 'denied');
-
-            // swap normal err and result to short-circuit the logical OR
-            if (err) {
-              return cb(undefined, err);
-            }
-
-            return cb(new Error('OK'));
-          });
-        }, function (ok, errors) { // note swapped results
-          var allowed = !_.isNull(ok) && ok.message === 'OK';
-
-          debug('    Request allowed: %s', allowed);
-
-          if (allowed) {
-            return next();
+            return sendSecurityError(errors[0], res, next);
           }
-
-          return sendSecurityError(errors[0], res, next);
-        });
+        );
       } else {
         return next();
       }
